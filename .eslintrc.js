0a041cf8f54853a8c49e2c71877bd41f7c7b776d
Taqtile <taqtile@Frozone.local>
2022-05-03 14:01:18 -0300

    eslint install

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  parser: "@typescriot-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
