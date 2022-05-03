2e227d8f2a715910f29e53dbae63b4ebdebe7b08
Taqtile <taqtile@Frozone.local>
2022-05-03 16:17:50 -0300

    database connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
-import { ApolloServer } from 'apollo-server';
+import { User } from './entity/User';
+import { DataSource } from 'typeorm';
 import 'reflect-metadata';
-import { schema } from './schema';
+// import { ApolloServer } from 'apollo-server';
+// import { schema } from './schema';
 
-export const server = new ApolloServer({
-  schema,
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'pimsil_local',
+  password: '1234',
+  database: 'pimsil_db_local',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
 });
 
-const port = 3030;
+AppDataSource.initialize()
+  .then(async () => {
+    console.log('Database Connection Successful');
+  })
+  .catch((error) => console.log(error));
 
-server.listen({ port }).then(({ url }) => {
-  console.log(`Server Started - URL: ${url}`);
-});
+//APOLLO SERVER CONNECTION TO USE LATER
+// export const server = new ApolloServer({
+//   schema,
+// });
+
+// const port = 3030;
+
+// server.listen({ port }).then(({ url }) => {
+//   console.log(`Server Started - URL: ${url}`);
+// });
