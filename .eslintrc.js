a31c61da352d1de722bc351c7503aee07b6638b3
Taqtile <taqtile@Frozone.local>
2022-05-03 14:29:20 -0300

    prettier config with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
   root: true,
   parser: "@typescriot-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
 };
