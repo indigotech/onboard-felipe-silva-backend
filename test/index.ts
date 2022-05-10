621b2c5b05ed4e5779b38c0c7f754ade3cf810b5
Taqtile <taqtile@Frozone.local>
2022-05-10 11:02:34 -0300

    rebase

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,32 @@ import { ApolloServer } from 'apollo-server';
 import { schema } from '../src/schema';
 import { expect } from 'chai';
 import { AppDataSource } from '../src/data-source';
+<<<<<<< HEAD
 import { initialSetup } from '../src';
+=======
+import { User } from '../src/entity/User';
+
+interface UserInput {
+  name: string;
+  birthDate: string;
+  email: string;
+  password: string;
+}
+
+interface MutationResponse {
+  id: number;
+  name: string;
+  birthDate: string;
+  email: string;
+}
+
+const testUser: UserInput = {
+  name: 'TestUser3',
+  birthDate: '09-06-1998',
+  email: 'testmail@test.com',
+  password: '1234567a',
+};
+>>>>>>> ffae8d4 (test create mutation)
 
 const port = process.env.APOLLO_PORT;
 const server = new ApolloServer({
@@ -29,5 +54,32 @@ describe('Queries Test', () => {
     const queryResult = axiosCall.data;
 
     expect(queryResult).to.be.deep.eq({ data: { hello: 'Hello World!' } });
+  });
+});
+
+describe('Mutation Test', () => {
+  it('Create Mutation', async () => {
+    const createUseMutation = await axios({
+      url,
+      method: 'post',
+      data: {
+        query: `
+          mutation CreateUser($credentials: UserInput!) {
+            createUser(data: $credentials) {
+              id,
+              name,
+              email,
+              birthDate
+            }
+          }
+        `,
+        variables: { credentials: testUser },
+      },
+    });
+
+    expect(createUseMutation.data.data.createUser.email).to.be.eq(testUser.email);
+    expect(createUseMutation.data.data.createUser.name).to.be.eq(testUser.name);
+    expect(createUseMutation.data.data.createUser.birthDate).to.be.eq(testUser.birthDate);
+    expect(!!createUseMutation.data.data.createUser.id).to.exist;
   });
 });
